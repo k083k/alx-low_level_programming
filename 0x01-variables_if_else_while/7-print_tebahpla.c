f43c29214681e99ddd19619ef9202e206b82be23
k083k <kobekore@gmail.com>
2022-03-11 09:25:48 -0800

    name change

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print lowarcase alphabets in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
