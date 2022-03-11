6ecc8ca4a3e53dc98fe22fdca640e8c4839653c4
k083k <kobekore@gmail.com>
2022-03-11 00:03:48 -0800

    4-print_alphabt.c completed with all betty suggestions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets except q and e followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+		{
+			putchar(alpha);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
