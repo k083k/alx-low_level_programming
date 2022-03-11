eba3477018f7c869e5465c48ccb18695f7aaca02
k083k <kobekore@gmail.com>
2022-03-11 00:52:33 -0800

    9-print_comb done and betty compliant

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single digit numbers
+ * separate number with a comma and should be arranged in
+ * ascending order
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+		if (digit != '9')
+		{
+			putchar(',');
+	        	putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
