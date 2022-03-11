a1b1e199d49e29da920d74b635a230ac5b85b4bc
k083k <kobekore@gmail.com>
2022-03-11 01:30:37 -0800

    100-print_comb3 completed and betty compliant

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - function thst prints all possible different combinations of
+ * two digits. separated by comma followed by space
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	char digit1;
+	char digit2;
+
+	for(digit1 = '0'; digit1 <= '9'; digit1++)
+	{
+		for (digit2 = '1'; digit2 <= '9'; digit2++)
+		{
+			if (digit1 < digit2)
+			{
+				putchar(digit1);
+				putchar(digit2);
+				if(digit1 != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
