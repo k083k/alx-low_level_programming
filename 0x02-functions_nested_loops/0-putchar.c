84caaca3523b5cb99acb9c69476df6aa787b0a8a
k083k <kobekore@gmail.com>
2022-03-15 02:26:44 -0700

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,24 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - This function prints out a string
+ * main - This function prints out a string followed
+ * by a new line
+ *
+ * Description: you are not allowed to use standard libraries
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	char p[] = "_putchar";
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(p[i]);
+	}
+	
+	_putchar('\n');
 	return (0);
 }
