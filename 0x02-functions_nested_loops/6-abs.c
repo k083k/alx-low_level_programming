f791bc15ce389dab87835cd4b5ebada8e7022717
k083k <kobekore@gmail.com>
2022-03-15 04:22:26 -0700

    6-abs.c Done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - calculates absolute value of input
+ * @int: input
+ * Return: return the absolute value
+ */
+
+int _abs(int c)
+{
+
+	if (c < 0)
+	{
+		c *= -1;
+	}
+	return (c);
+}
