c14cb7bd232705fe8692bc86090692babbe198f1
k083k <kobekore@gmail.com>
2022-03-15 02:37:26 -0700

    2-print_alphabet_x10.c correction made

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
-/*
- * print_alphabet_x10 - prints alphabets 10 times on
- * seoarate lines
+/**
+ * print_alphabet_x10 - prints alphabets 10 times on separate lines
+ *
  */
 
 void print_alphabet_x10(void)
 {
 	char alpha;
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
