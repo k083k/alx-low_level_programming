8decefcfc0f29b0ac2cb6a134fd7a96276557742
k083k <kobekore@gmail.com>
2022-03-16 00:35:56 -0700

    2-largest_number.c Finished. Betty compliant

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if ((a > b && b > c) || (a > c && c > b) || (b == c && c < a))
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if ((b > a && a > c) || (c > a && b > c) || (a == b && b > c))
 	{
 		largest = b;
 	}
