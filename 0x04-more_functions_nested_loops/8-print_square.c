76a1464abfd300f74d81147cd73c9f5e89e25314
k083k <kobekore@gmail.com>
2022-03-17 11:50:31 -0700

    8-print_square.c Corrected

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,11 +9,7 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size == 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (size != 0)
 	{
 		for (i = 1; i <= size; i++)
 		{
@@ -23,6 +19,9 @@ void print_square(int size)
 			}
 			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
