b93919804736eeb3cf93d94274deeb26be791e47
k083k <kobekore@gmail.com>
2022-03-21 09:25:18 -0700

    Task 7-puts_half.c Done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: input/parameter
+ */
+
+void puts_half(char *str)
+{
+	int i, len, half;
+	len = 0;
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	if (len % 2 != 0)
+	{
+		half = (len - 1) / 2;
+	}
+	else
+	{
+		half = len / 2;
+	}
+	
+	for (i = half; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
