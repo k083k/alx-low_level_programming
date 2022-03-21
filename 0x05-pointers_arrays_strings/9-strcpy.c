7e2b974d77fcca42f2dcfc65cd7c2ad9d973b148
k083k <kobekore@gmail.com>
2022-03-21 11:11:16 -0700

    9-strcpy.c Finished

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,13 +11,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = -1;
 
-	i = 0;
-	while (src[i] <= '\0')
-	{
-		dest[i] = src[i];
+	do {
 		i++;
-	}
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
 	return (dest);
 }
