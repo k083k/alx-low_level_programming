ef1caec31c3c44e973062582b6f5470a59288495
k083k <kobekore@gmail.com>
2022-03-21 09:38:43 -0700

    7-puts_half.c Corrected

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,8 +8,9 @@
 void puts_half(char *str)
 {
 	int i, len, half;
+
 	len = 0;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		len++;
@@ -23,7 +24,7 @@ void puts_half(char *str)
 	{
 		half = (len / 2);
 	}
-	
+
 	for (i = half; i < len; i++)
 	{
 		_putchar(str[i]);
