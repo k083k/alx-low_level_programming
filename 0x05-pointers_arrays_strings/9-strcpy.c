35b3e2ef2e9cf33bc4e2e906667f97d896f78bdc
k083k <kobekore@gmail.com>
2022-03-21 11:04:10 -0700

    9-strcpy.c Finished

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcpy - copies a string
@@ -13,9 +14,10 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	i = 0;
-	for  (i = 0; src[i] != '\0'; i++)
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
 	return (dest);
 }
