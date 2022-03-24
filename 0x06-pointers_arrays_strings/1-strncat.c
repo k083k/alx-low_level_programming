0e34ed766544f030788abe43414e5f2893a2d0dd
k083k <kobekore@gmail.com>
2022-03-24 05:42:27 -0700

    Task 1-strncat Completed

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates one string with part of another string
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: destination String
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, y;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+
+	for (y = 0; src[y] != src[n]; y++)
+	{
+		dest[i + y] = src[y];
+	}
+	dest[i + n] = '\0';
+
+	return (dest);
+}
