b69de2bbaba343bb2f653a875d3d5ccf99e7ef95
k083k <kobekore@gmail.com>
2022-03-28 04:45:11 -0700

    Task 4 Completed

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - locates occurence of byte in string
+ * @s: string
+ * @accept: contains bytes 
+ *
+ * Return: pointer to matching byte
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return NULL;
+}
