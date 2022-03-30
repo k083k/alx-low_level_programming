e74c578e8a3d9fb11c66e43f43549c53297a4bf6
k083k <kobekore@gmail.com>
2022-03-30 02:30:38 -0700

    Task 3 Finished

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - calculates the factorial of a number
+ * @n: input
+ *
+ * Return: factorial result
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}
