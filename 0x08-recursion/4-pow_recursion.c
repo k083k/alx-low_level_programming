a65b577c18a589f4ff5ed214d739c649587521b6
k083k <kobekore@gmail.com>
2022-03-30 03:31:31 -0700

    Task 4 Completed

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns resutl of x to th epower y
+ * @x: integer
+ * @y: exponent
+ *
+ * Returnn: result of operation
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int pow;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	pow = x * _pow_recursion(x, (y - 1));
+	return (pow);
+}
