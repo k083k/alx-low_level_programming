f3cbcc66f8442a41a0f6927710aeee0720faf1cd
k083k <kobekore@gmail.com>
2022-04-01 02:38:36 -0700

    Task 0 Finished

---
+++ 0x0A-argc_argv.c/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that outputs its name followed by new line
+ * @argc: parameter/argument count
+ * @argv: argument vector
+ *
+ * Return: ALways 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
