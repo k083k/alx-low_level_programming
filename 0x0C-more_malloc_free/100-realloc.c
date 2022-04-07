a66320de5352d64e497d9426891c7242da4cc19e
k083k <kobekore@gmail.com>
2022-04-07 05:46:56 -0700

    Task 4 Finished

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,31 +31,33 @@ void _memcpy(void *dest, void *src, size_t n)
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 
-void *newPtr;
-if (new_size == 0)
-{
-	if (ptr != NULL)
-		free(ptr);
-	return (NULL);
-}
-else if (!ptr)
-{
-	return (malloc(new_size));
-}
-else if (new_size <= old_size)
-{
-	return (ptr);
-}
-else
-{
-	newPtr = malloc(new_size);
-	if (newPtr)
+	void *newPtr;
+	if (new_size == 0)
 	{
-		_memcpy(newPtr, ptr, old_size);
-		free(ptr);
+		if (ptr != NULL)
+		{
+			free(ptr);
+		}
+		return (NULL);
 	}
-	return (newPtr);
-}
+	else if (!ptr)
+	{
+		return (malloc(new_size));
+	}
+	else if (new_size <= old_size)
+	{
+		return (ptr);
+	}
+	else
+	{
+		newPtr = malloc(new_size);
+		if (newPtr)
+		{
+			_memcpy(newPtr, ptr, old_size);
+			free(ptr);
+		}
+		return (newPtr);
+	}
 
-return (0);
+	return (0);
 }
