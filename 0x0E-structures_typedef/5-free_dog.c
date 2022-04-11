95075917d654dfd8053595bbd1f8bca1e15bab9e
k083k <kobekore@gmail.com>
2022-04-11 05:20:21 -0700

    Task 5 Corrected

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -11,12 +11,11 @@
 
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return;
+		free(d->name);
+		free(d->owner);
+		free(d);
 	}
 	
-	free(d->name);
-	free(d->owner);
-	free(d);
 }
