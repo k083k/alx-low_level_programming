b80433b29d7872d71a882178f163a6bd87ac961f
k083k <kobekore@gmail.com>
2022-04-11 05:40:37 -0700

    Correction

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,16 @@
  * Return: Nothing (Void)
  */
 
-
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
+
+	if (!d->name)
+		d->name = "(nil)";
+	if (!d->owner)
+		d->owner = "(nil)";
+
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
