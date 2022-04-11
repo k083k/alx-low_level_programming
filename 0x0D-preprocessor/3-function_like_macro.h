92d634f71fb0517e3602844cdbfa53382365eb58
k083k <kobekore@gmail.com>
2022-04-11 05:53:18 -0700

    Corrected

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,7 +2,7 @@
 #define FUNCTION_LIKE_MACRO_H
 /**
  * ABS - function macro
- * @x: Argument to be passed
+ * @X: Argument to be passed
  *
  * Desc: Header file that defines a funtion-like macro ABS(X)
  *		that computes the absolute value of the number x.
