4e8b43352857bdeb545be7851f71f706f7e4e81e
k083k <kobekore@gmail.com>
2022-04-11 04:14:58 -0700

    Task 5 Done

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * free_dog - Frees dogs
+ * @d: pointer to dog to be freed
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
