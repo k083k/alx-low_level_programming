9fe8393ad1e5ef88c4aaa35532108db548254dfc
k083k <kobekore@gmail.com>
2022-04-25 12:46:57 -0700

    Task 0 Finished

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_listint - return the number of nodes in singlylinked list
+ * @h: singly linked list
+ *
+ * Return: Number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
