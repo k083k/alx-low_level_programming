9b1bbcfe53c93ed1df52b85669eb0755b5a304cd
k083k <kobekore@gmail.com>
2022-06-09 09:21:44 -0700

    Task 7 Completed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,45 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - the function
+ * insert_dnodeint_at_index - inserts a node at a given index
+ * in a doubly linked list
+ * @h: double pointer to the list
+ * @idx: index of the node to insert
+ * @n: data to insert
  *
- * @h: the pointer
- * @idx: the node wanted
- * @n: data
- * Return: new node
+ * Return: address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	/* allow memory*/
-	dlistint_t *prev, *new_node = new_node;
-	unsigned int i = 1;
+	unsigned int i;
+	dlistint_t *new;
+	dlistint_t *temp = *h;
 
-	new_node = malloc(sizeof(dlistint_t));
+	new = malloc(sizeof(dlistint_t));
+	if (!new || !h)
+		return (NULL);
 
-	/* set prev head*/
-	prev = (*h);
-	/* in case of fail*/
-	if (new_node == NULL)
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (i = 0; temp && i < idx; i++)
 	{
-		return (NULL);
-	}
-	if ((*h) == NULL)
-	{
-		return (NULL);
-	}
-	/* set next of newNode*/
-	while (i <= idx)
-	{
-		if (i == idx)
+		if (i == idx - 1)
 		{
-			new_node->n = n;
-			new_node->next = prev->next;
-			prev->next = new_node;
-			new_node->prev = prev;
-			new_node->next->prev = new_node;
-			return (new_node);
+			if (temp->next == NULL)
+				return (add_dnodeint_end(h, n));
+			new->next = temp->next;
+			new->prev = temp;
+			temp->next->prev = new;
+			temp->next = new;
+			return (new);
 		}
-		prev = prev->next;
-		i++;
+		else
+			temp = temp->next;
 	}
+
 	return (NULL);
 }
