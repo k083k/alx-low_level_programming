dafd6b2b2283500e23c1807d548c853628bf5209
k083k <kobekore@gmail.com>
2022-10-04 10:18:30 -0700

    Tasks

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - Jump search algo
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: value to be searched
+ *
+ * Return: index where value is located, or -1 on failure
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int jump = (int)sqrt(size);
+	size_t i = 0, j;
+
+	if (array == NULL || size == 0)
+		return (-1);
+	while (array[i] < value && i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		i += jump;
+	}
+	if (i)
+		printf("Value found between indexes [%lu] and [%lu]\n", i - jump, i);
+	else
+	{
+		printf("Value found between indexes [%lu] and [%d]\n", i, jump);
+		i = size;
+	}
+	j = i - jump;
+	for (; j <= i && j < size; j++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", j, array[j]);
+		if (array[j] == value)
+			return (j);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,47 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - interpolation search algo
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: value to be searched
+ *
+ * Return: index where value is located, or -1 on failure
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t pos;
+
+	if (array == NULL)
+		return (-1);
+
+	pos = get_pos(array, 0, (int) size - 1, value);
+	while (pos < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return ((int) pos);
+		else if (array[pos] > value)
+			pos = get_pos(array, 0, pos - 1, value);
+		else
+			pos = get_pos(array, pos + 1, (int) size - 1, value);
+	}
+	printf("Value checked array[%lu] is out of range\n", pos);
+	return (-1);
+}
+
+/**
+ * get_pos - applies linear interpolation formula
+ * @array: array to be used
+ * @low: lowest index in the array to be used
+ * @high: highest index in the array to be used
+ * @value: value being searched
+ *
+ * Return: probable position of value
+ */
+size_t get_pos(int *array, int low, int high, int value)
+{
+	double slope_r = ((double)(high - low) / (array[high] - array[low]));
+
+	return (low + (slope_r * (value - array[low])));
+}
